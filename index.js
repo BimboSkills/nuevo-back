261705892ec085be0a1217b493b0b64af346d4f9
BimboSkills <cristiannivaalvarez@gmail.com>
2023-07-05 20:42:59 -0300

    agregado de .env + port

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require ('dotenv').config()
 const http = require ("http")
 
 function requestController() {
     console.log("Recibimos una request!")
 }
-
+// Config del server
 const server = http.createServer(requestController)
 
-server.listen(4000)
+const PORT = process.env.PORT
+server.listen(PORT, function() {
+    console.log("Aplicacion corriendo en puerto: node" + PORT")
+})

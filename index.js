21f4a48c27aa14144f9052e538617f9a13caad1e
BimboSkills <cristiannivaalvarez@gmail.com>
2023-07-06 00:46:37 -0300

    method get if

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 require ('dotenv').config()
 const http = require ("http")
 
-function requestController() {
-    console.log("Recibimos una request!")
+function requestController(req,res) {
+cl
+    const url = req.url
+    const method = req.method
+    console.log({url, method})
+
+    if (method === "GET" && url ==="/") {
+        res.setHeader ("Content-type", "text/html", "charset=utf-8")
+        res.write ("<h1> Hola mundo desde la pagina PRINCIPAL </h1>")
+        res.end()
+        return
+    }
+
+    if (method === "GET" && url === "/about") {
+        res.setHeader("Content-type", "text/html")
+        res.write("<h1>Hola mundo desde la pagina ABOUT</h1>")
+        res.end()
+        return
+    }
+
+    res.setHeader("Content-type", "text/html; charset=utf-8")
+    res.write ("<h1>Pagina no encontrada</h1>")
+    res.end()
 }
 // Config del server
 const server = http.createServer(requestController)
 
 const PORT = process.env.PORT
 server.listen(PORT, function() {
-    console.log("Aplicacion corriendo en puerto: node" + PORT")
+    console.log("Aplicacion corriendo en puerto: " + PORT)
 })
